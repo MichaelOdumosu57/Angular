78daebc08a7d48868d8a5c458b1d41c111d76533
Michael Odumosu <michaelodumosu57@gmail.com>
2019-09-05 21:21:08 -0400

    learned about code coverage in angular

--- libraries/magic-strings/projects/magic-string/src/lib/magic-string.component.ts
+++ libraries/magic-strings/projects/magic-string/src/lib/magic-string.component.ts
@@ -3,17 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'nk-magic-string',
   template: `
-    <p>
-      magic-string works!
-    </p>
+    <input type="text" (keydown.enter) = "markText($event.target.value)"/>
+    <div #content [hidden]="true">
+        <ng-content></ng-content>
+    </div>
+    <div [innerHTML]="controlledContent"></div>
   `,
-  styles: []
+  styles: ['.mark {background-color:yellow}'],
+//   encapsulation:ViewEncapsulation.None
 })
 export class MagicStringComponent implements OnInit {
+    
+    @ViewChild('content',{read:ViewContainerRef}) content: ElementRef;
+    
+    originalContent:string;
+    controlledContent:string;
+    
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.controlledContent = this.originalContent = this.content.nativeElement.textContent
+        this.controlledContent = this.originalContent.replace(new Regexp(value,'g'),`<span class ="mark>$(value)</span>`)
+    }
+    
+    markText(value:any){
+        console.log(any)
+    }
 
 }
+
+// hide content render something else
+
+// use ViewChild to help look at DOM element telplates in
+    //second can read or override it or use extra API
+    
+//to hide an element    <div  [hidden]="true">
